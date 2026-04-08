97a1a8d1ce6ae9977af3b9a213c973f516fa3947
lucasuchoa01 <lucasuchoa197@gmail.com>
2026-04-08 15:35:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import Layout from './components/Layout'
 import Hoje from './pages/Hoje'
 import LimparCabeca from './pages/LimparCabeca'
 import Direcao from './pages/Direcao'
 import Evolucao from './pages/Evolucao'
 
-// App sem login — uso individual
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Hoje />} />
-          <Route path="/limpar" element={<LimparCabeca />} />
-          <Route path="/direcao" element={<Direcao />} />
-          <Route path="/evolucao" element={<Evolucao />} />
-          <Route path="*" element={<Hoje />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Hoje />} />
+            <Route path="/limpar" element={<LimparCabeca />} />
+            <Route path="/direcao" element={<Direcao />} />
+            <Route path="/evolucao" element={<Evolucao />} />
+            <Route path="*" element={<Hoje />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
