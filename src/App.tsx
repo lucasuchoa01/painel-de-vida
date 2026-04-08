eaf28bd25483f4ddcf654888a403b4820317a7bd
lucasuchoa01 <lucasuchoa197@gmail.com>
2026-04-08 18:10:50 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { useAuth } from './context/AuthContext'
 import Layout from './components/Layout'
+import Login from './pages/Login'
 import Hoje from './pages/Hoje'
 import LimparCabeca from './pages/LimparCabeca'
 import Direcao from './pages/Direcao'
 import Evolucao from './pages/Evolucao'
 
+function PrivateRoutes() {
+  const { user } = useAuth()
+  if (!user) return <Login />
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Hoje />} />
+        <Route path="/limpar" element={<LimparCabeca />} />
+        <Route path="/direcao" element={<Direcao />} />
+        <Route path="/evolucao" element={<Evolucao />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Layout>
+  )
+}
+
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Hoje />} />
-            <Route path="/limpar" element={<LimparCabeca />} />
-            <Route path="/direcao" element={<Direcao />} />
-            <Route path="/evolucao" element={<Evolucao />} />
-            <Route path="*" element={<Hoje />} />
-          </Routes>
-        </Layout>
+        <PrivateRoutes />
       </BrowserRouter>
     </AuthProvider>
   )
